fdb5eb3621472d89e96312bd3b3e42f90831d829
frenchi123 <frenchi.craff@gmail.com>
2026-05-01 19:16:54 -0300

    fix: clean repo and update events flow

--- Application/UseCases/Events/Handlers/GetEventsHandler.cs
+++ Application/UseCases/Events/Handlers/GetEventsHandler.cs
@@ -7,6 +7,7 @@ using Application.DTOs;
 using Application.Interfaces;
 using Application.UseCases.Events.Queries;
 using Domain.Entities;
+using static System.Net.WebRequestMethods;
 
 namespace Application.UseCases.Events.Handlers
 {
@@ -27,8 +28,8 @@ namespace Application.UseCases.Events.Handlers
             {
                 Id = e.Id,
                 Name = e.Name,
-                EventDate = e.EventDate,
-            //    ImangenUrl= e.ImageUrl,    por si llegamos con la imagen
+                EventDate = e.EventDate, // no dimos con el tiempo y en ves de cargar la url en el evento y levantarla de la base la pusimos aca
+                ImangenUrl = "https://media.ambito.com/p/7779360f0b5ae7022756553912b7b626/adjuntos/239/imagenes/038/942/0038942625/la-rengajpg.jpg",
                 Sectors = e.SectorsList?.Select(s => new SectorResponse
                 {
                     Id = s.Id,
